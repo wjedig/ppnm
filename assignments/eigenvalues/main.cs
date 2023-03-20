ba11d87636ae720d3e226719a119eab3e20f4fb8
William Jedig <jedjedjedig@gmail.com>
2023-03-20 11:52:24 +0100

    Working on assignment 2

--- assignments/eigenvalues/main.cs
+++ assignments/eigenvalues/main.cs
@@ -17,6 +17,21 @@ public static class Utils{
 		}
 		return A;
 	}
+	public static matrix build_hamiltonian(double rmax, double dr){
+		int npoints = (int)(rmax/dr)-1;
+		vector r = new vector(npoints);
+		for(int i=0 ; i<npoints ; i++){r[i]=dr*(i+1);}
+		matrix H = new matrix(npoints,npoints);
+		for(int i=0 ; i<npoints-1 ; i++){
+			H[i,i] = -2;
+			H[i,i+1] = 1;
+			H[i+1,i] = 1;
+		}
+		H[npoints-1,npoints-1] = -2;
+		matrix.scale(H,-(0.5/dr/dr));
+		for(int i=0;i<npoints;i++){H[i,i]+=-1/r[i];}
+		return H;
+	}
 }
 
 public static class Test{
@@ -37,20 +52,35 @@ public static class Test{
 		WriteLine($"VTV = I : {VTV.approx(ID)}");
 		matrix VVT = V*(V.T);
 		WriteLine($"VVT = I : {VVT.approx(ID)}");
-
+	}
+	public static void testB_eigenvals(double rmax, double dr){
+		matrix ham = Utils.build_hamiltonian(rmax, dr);
+		(vector e, matrix V) = jacobi.cyclic(ham);
+		e.print("Eigenvalues given by:");
+		V.print("With corresponding Eigenvectors given by:");
+	}
+	public static void testB_rmax(double rmax, double dr){
+		matrix ham = Utils.build_hamiltonian(rmax, dr);
+		(vector e, matrix V) = jacobi.cyclic(ham);
+		WriteLine($"{rmax} {e[0]}");
+	}
+	public static void testB_dr(double rmax, double dr){
+		matrix ham = Utils.build_hamiltonian(rmax, dr);
+		(vector e, matrix V) = jacobi.cyclic(ham);
+		WriteLine($"{dr} {e[0]}");
 	}
 }
 
 class main{
 	public static void Main(string[] args){
-		int rmax = 10;
+		double rmax = 10;
 		double dr = 0.3;
 		int size = 10;
 		double scale = 100.0;
 		bool trace = false;
 		foreach(string param in args){ // set all params
 			string[] words = param.Split(":");
-			if(words[0] == "-rmax"){rmax = int.Parse(words[1]);}
+			if(words[0] == "-rmax"){rmax = double.Parse(words[1]);}
 			if(words[0] == "-dr"){dr = double.Parse(words[1]);}
 			if(words[0] == "-size"){size = int.Parse(words[1]);}
 			if(words[0] == "-scale"){scale = double.Parse(words[1]);}
@@ -58,7 +88,9 @@ class main{
 		}
 		foreach(string run in args){
 			if(run == "-test"){Test.testA(size,scale,trace);}
-			if(run == "-hydrogen"){Test.testA();}
+			if(run == "-hydrogen"){Test.testB_eigenvals(rmax, dr);}
+			if(run == "-plotrmax"){Test.testB_rmax(rmax, dr);}
+			if(run == "-plotdr"){Test.testB_dr(rmax, dr);}
 
 		}
 	}
